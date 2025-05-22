b75ba617da26b6dca5cfad10c0d53905d7b38699
Tom Van Manen <tom.on.github@gmail.com>
2025-05-21 22:16:44 -0700

    Install tailwindcss
    largely following https://ulfschneider.io/2025-05-20-tailwind-css-and-11ty/

---
+++ eleventy.config.mjs
@@ -0,0 +1,26 @@
+// from https://ulfschneider.io/2025-05-20-tailwind-css-and-11ty/
+import { execSync } from "node:child_process";
+
+export default function (eleventyConfig) {
+
+  //run the CSS preparation before each Eleventy build
+  eleventyConfig.on(
+    "eleventy.before",
+    ({ dir, results, runMode, outputMode }) => {
+      prepareTailwind();
+    },
+  );
+
+  //generate the actual CSS for the website
+  function prepareTailwind() {
+    console.log("Prepare Tailwind CSS");
+    execSync("npm run build:css", {
+      cwd: "./",
+      encoding: "utf-8",
+      stdio: "inherit",
+    });
+  }
+
+  //Whenever the generated CSS changes, copy the CSS to its final destination
+  eleventyConfig.addPassthroughCopy({ "_code/_css/style.css": "css" });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./_code/**/*",
+    "!./_code/_css/**/*",
+    "./content/**/*"
+  ]
+}
